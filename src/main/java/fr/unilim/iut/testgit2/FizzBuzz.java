8caf942644826f4e7e0f2cdecc025610c1eced99
Victor Maintenant <vic.maint@outlook.fr>
2019-04-02 16:44:52 +0200

    Ajout FizzBuzz de 1 à 15

---
+++ src/main/java/fr/unilim/iut/testgit2/FizzBuzz.java
@@ -0,0 +1,8 @@
+package fr.unilim.iut.testgit2;
+
+public class FizzBuzz {
+	public static String getResult() {
+	    return "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz";
+    }
+
+}
--- src/main/java/fr/unilim/iut/testgit2/HelloWorld.java
+++ src/main/java/fr/unilim/iut/testgit2/HelloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World !");
 		System.out.println("Jouons ensemble !");
+		System.out.println("Et si on jouait au FizzBuzz ?");
+		System.out.println(FizzBuzz.getResult());
 	}
 }
