288f58bebd18f8d23ed45f2bd6dfdacf53bc1544
Victor Maintenant <vic.maint@outlook.fr>
2019-04-02 17:08:01 +0200

    Ajout Marabout simple 3 mots

--- src/main/java/fr/unilim/iut/testgit2/HelloWorld.java
+++ src/main/java/fr/unilim/iut/testgit2/HelloWorld.java
@@ -9,5 +9,7 @@ public class HelloWorld {
 		System.out.println(FizzBuzz.getResult());
 		System.out.println( "Et si on jouait au Marabout ?" );  
         System.out.println(Marabout.getResult());
+        System.out.println( "Et si on jouait au Marabout de manière plus simple ?" );  
+        System.out.println(MaraboutSimple.getResult());
 	}
 }
---
+++ src/main/java/fr/unilim/iut/testgit2/MaraboutSimple.java
@@ -0,0 +1,8 @@
+package fr.unilim.iut.testgit2;
+
+public class MaraboutSimple {
+	 public static String getResult() {
+         return "Marabout, Bout de ficelle, Selle de cheval";
+   }
+
+}
