cc608b5a11a44c369c0ea4f9c90f96aa2ec06c74
Victor Maintenant <vic.maint@outlook.fr>
2019-04-02 16:58:43 +0200

    Suite Marabout avec 3 phrases de plus

--- src/main/java/fr/unilim/iut/testgit2/HelloWorld.java
+++ src/main/java/fr/unilim/iut/testgit2/HelloWorld.java
@@ -7,5 +7,7 @@ public class HelloWorld {
 		System.out.println("Jouons ensemble !");
 		System.out.println("Et si on jouait au FizzBuzz ?");
 		System.out.println(FizzBuzz.getResult());
+		System.out.println( "Et si on jouait au Marabout ?" );  
+        System.out.println(Marabout.getResult());
 	}
 }
